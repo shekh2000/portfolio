7a6b88ff6250bcfd2dd422ef51d27f2120a2ffcd
shekh2000 <shekhawatdeepak13@gmail.com>
2023-10-13 17:47:31 +0530

    second commit

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -11,7 +11,7 @@ const Intro = () => {
     // <img src={btnImg} alt = 'Hire me' />
         <section id='intro'>
             <div className='introContent'>
-                <span className='hello'>Hello, mere chahaane waalo</span><br/>
+                <span className='hello'>Hello, mera chahaane waalo</span><br/>
                 <span className='introText'>I' m <span className='introName'>Deepak Singh</span><br/>Web Developer</span>
                 <p className='introPara'>As a computer engineer, I thrive on the symphony of algorithms and circuits,<br/> sculpting the future of technology with precision and innovation.</p>
                 <Link><button className='btn'><img src={btnImg} alt = 'Hire me' className='btnImg'/>Hire Me</button></Link>
