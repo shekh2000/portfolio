518153d84c377d0cb000af0eb0cd291d3032fed6
shekh2000 <shekhawatdeepak13@gmail.com>
2023-10-23 16:26:05 +0530

    updated skills section

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -12,33 +12,19 @@ const skills = () => {
   return (
     <section id='skills'>
         <span className='skillTitle'>What i do</span>
-        <span className='skillDesc'>I am a fresher and have experience in various skills as mentioned below</span>
-        {/* <div className='skillSets'>
+        <span className='skillDesc'>I am a fresher, and have worked on multiple project during my acadmics, and have experience in various skills as mentioned below.I have also worked in AI/ML and have good grasp on computer science fundamentals.</span>
+        <div className='skill_upper_part'>
             <img src={html} alt='' className='htmlImg' />
-            <img src={javasript} alt='' className='javasriptImg' />
+            <img src={javascript} alt='' className='javascriptImg' />
             <img src={git} alt='' className='gitImg' />
             <img src={docker} alt='' className='dockerImg' />
+            </div>
+
+        <div className='skill_lower_part'> 
             <img src={mongodb} alt='' className='mongodbImg' />
             <img src={style} alt='' className='styleImg' />
             <img src={react} alt='' className='reactImg' />
-        </div> */}
-        <div className="skillSets">
-        <div className="row">
-          <img src={html} alt="" className="htmlImg" />
-          <img src={javascript} alt="" className="javascriptImg" />
-        </div>
-        <div className="row">
-          <img src={git} alt="" className="gitImg" />
-          <img src={docker} alt="" className="dockerImg" />
-        </div>
-        <div className="row">
-          <img src={mongodb} alt="" className="mongodbImg" />
-          <img src={style} alt="" className="styleImg" />
-        </div>
-        <div className="row">
-          <img src={react} alt="" className="reactImg" />
-        </div>
-      </div>
+            </div>
     </section>
   )
 }
